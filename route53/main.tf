8040a198c01b50125ea2a20465dcc7d83411212d
policyoftruth <ewinningham@gmail.com>
2021-06-26 12:19:43 -0400

    readability

--- route53/main.tf
+++ route53/main.tf
@@ -1,18 +1,3 @@
-variable "domain_name" {
-  type = string
-  default = "winningham.me"
-}
-
-variable "k8s_sub" {
-  type = string
-  default = "k8s.winningham.me"
-}
-
-variable "aws_region" {
-  type = string
-  default  = "us-east-1"
-}
-
 resource "aws_route53_zone" "primary" {
   name = var.domain_name
 }
@@ -33,6 +18,21 @@ resource "aws_route53_record" "domain_ns" {
     aws_route53_zone.primary.name_servers[1],
     aws_route53_zone.primary.name_servers[2],
     aws_route53_zone.primary.name_servers[3],
+  ]
+}
+
+resource "aws_route53_record" "k8s_domain_ns" {
+  allow_overwrite = true
+  name            = var.k8s_sub
+  ttl             = 300
+  type            = "NS"
+  zone_id         = aws_route53_zone.primary.zone_id
+
+  records = [
+    aws_route53_zone.k8s.name_servers[0],
+    aws_route53_zone.k8s.name_servers[1],
+    aws_route53_zone.k8s.name_servers[2],
+    aws_route53_zone.k8s.name_servers[3],
   ]
 }
 
@@ -59,23 +59,4 @@ resource "aws_route53_record" "www_cname" {
 
 output "ns_output" {
   value = aws_route53_zone.primary.name_servers
-}
-
-resource "aws_route53_record" "k8s_domain_ns" {
-  allow_overwrite = true
-  name            = var.k8s_sub
-  ttl             = 300
-  type            = "NS"
-  zone_id         = aws_route53_zone.primary.zone_id
-
-  records = [
-    aws_route53_zone.k8s.name_servers[0],
-    aws_route53_zone.k8s.name_servers[1],
-    aws_route53_zone.k8s.name_servers[2],
-    aws_route53_zone.k8s.name_servers[3],
-  ]
-}
-
-output "k8s_ns_output" {
-  value = aws_route53_zone.k8s.name_servers
 }
---
+++ route53/variables.tf
@@ -0,0 +1,14 @@
+variable "domain_name" {
+  type = string
+  default = "winningham.me"
+}
+
+variable "k8s_sub" {
+  type = string
+  default = "k8s.winningham.me"
+}
+
+variable "aws_region" {
+  type = string
+  default  = "us-east-1"
+}
