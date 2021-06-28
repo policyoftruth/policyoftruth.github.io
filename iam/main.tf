85780892bd4b0e23694eedb7cf59b2e972d1f1ac
policyoftruth <ewinningham@gmail.com>
2021-06-27 22:02:59 -0400

    added iam stuff

---
+++ iam/main.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_group" "kops_group" {
+  name = "kops"
+}
+
+resource "aws_iam_group_policy_attachment" "kops_group_policy_attach" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+  ])
+  group      = aws_iam_group.kops_group.name
+  policy_arn = each.key
+}
+
+resource "aws_iam_user" "kops_user" {
+  name = "kops"
+}
+
+resource "aws_iam_access_key" "kops_user_key" {
+  user = aws_iam_user.kops_user.name
+}
+
+resource "aws_iam_user_group_membership" "kops_user_group" {
+  user = aws_iam_user.kops_user.name
+
+  groups = [
+    aws_iam_group.kops_group.name,
+  ]
+}
+
+output "kops_user_secret" {
+  sensitive = true
+  value = aws_iam_access_key.kops_user_key.secret
+}
