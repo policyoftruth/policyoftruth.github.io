66c51047bf052a5ee1e7ea14aaab15fce8ca8314
policyoftruth <ewinningham@gmail.com>
2021-11-12 09:24:15 -0500

    fake push

--- route53/variables.tf
+++ route53/variables.tf
@@ -3,11 +3,6 @@ variable "domain_name" {
   default = "winningham.me"
 }
 
-variable "k8s_sub" {
-  type    = string
-  default = "k8s.winningham.me"
-}
-
 variable "aws_region" {
   type    = string
   default = "us-east-1"
