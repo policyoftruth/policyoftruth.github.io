a6b43f43b19b97bb27757ff9fe088cfb5339f415
nulldivision <gauge@757.org>
2025-12-13 17:16:42 -0500

    refactor: separate tf vars from definitions

--- route53/variables.tf
+++ route53/variables.tf
@@ -1,9 +1,9 @@
 variable "domain_name" {
-  type    = string
-  default = "winningham.me"
+  type        = string
+  description = "The primary domain name for the website (e.g. example.com)"
 }
 
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+  type        = string
+  description = "The AWS region to deploy resources into"
 }
